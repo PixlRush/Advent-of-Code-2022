37bfe60525ff16f74b688707b842158191020d92
Matthew Rappaport <catfan348@hotmail.com>
2022-12-06 01:15:20 -0500

    The lambda is un-needed

--- 06/02.py
+++ 06/02.py
@@ -4,7 +4,7 @@ with open("input.txt", 'r') as theFile:
     theLine = theFile.readline()
     for i in range(14, len(theLine)):
         window = theLine[i-14:i]
-        maxinst = max(map(lambda x: window.count(x), window))
+        maxinst = max(map(window.count, window))
         if maxinst == 1:
             index = i
             break
