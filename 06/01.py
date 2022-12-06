0e80ea9d103e8975513c26d23b9974f5f9c87875
Matthew Rappaport <catfan348@hotmail.com>
2022-12-06 00:29:40 -0500

    Solved Problem 1

---
+++ 06/01.py
@@ -0,0 +1,12 @@
+index = 0
+
+with open("input.txt", 'r') as theFile:
+    theLine = theFile.readline()
+    for i in range(4, len(theLine)):
+        window = theLine[i-4:i]
+        maxinst = max(map(lambda x: window.count(x), window))
+        if maxinst == 1:
+            index = i
+            break
+
+print(index)
